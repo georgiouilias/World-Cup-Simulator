d928a466d16045fd875cedc557f9b3f4d643ef84
GeorgiouIlias <66827818+georgiouilias@users.noreply.github.com>
2020-07-11 22:01:01 +0300

    patch 6

--- World Cup/src/FIFAPoints.java
+++ World Cup/src/FIFAPoints.java
@@ -4,17 +4,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class FIFAPoints {
-	public static void GroupStagePoints() {
-		
-		double resultH = 0;
-		double resultA = 0;
+	public static void setPoints(int id, int important, double resultH, double resultA) {
 
-		for (int id = 101; id <= 102; id++) {
 
 			String q1 = "SELECT * FROM schedule WHERE Match_ID= " + id;
-			String h = "SELECT * FROM fifaranking2 WHERE Name=?";
-			String a = "SELECT * FROM fifaranking2 WHERE Name=?";
-			String u = "UPDATE `fifaranking2` SET `Points` = ? WHERE `fifaranking2`.`Name` = ?";
+			String h = "SELECT * FROM fifaranking WHERE Name=?";
+			String a = "SELECT * FROM fifaranking WHERE Name=?";
+			String u = "UPDATE `fifaranking` SET `Points` = ? WHERE `fifaranking`.`Name` = ?";
 
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
@@ -31,80 +27,46 @@ public class FIFAPoints {
 
 				while (rs1.next()) {
 
-						System.out.println();
-						System.out.println("Match: " +id);
-						System.out.println(rs1.getString(2) + " vs " + rs1.getString(3));
+					System.out.println();
+					System.out.println("Match: " + id);
+					System.out.println(rs1.getString(2) + " vs " + rs1.getString(3));
 
-						psth.setString(1, rs1.getString(2));
-						ResultSet hrs = psth.executeQuery();
+					psth.setString(1, rs1.getString(2));
+					ResultSet hrs = psth.executeQuery();
 
-						psta.setString(1, rs1.getString(3));
-						ResultSet ars = psta.executeQuery();
+					psta.setString(1, rs1.getString(3));
+					ResultSet ars = psta.executeQuery();
 
-						while(hrs.next()) {
-							while(ars.next()) {
-							
-							if(rs1.getInt(4)>rs1.getInt(5)) {
-								System.out.println("Winner is:" + hrs.getString(2));
-								resultH = 1;
-								resultA = 0;
-	
-							}else if(rs1.getInt(4)==rs1.getInt(5)) {
-								System.out.println("Draw");
-								resultH = 0.5;
-								resultA = 0.5;
-								
-							}else {
-								System.out.println("Winner is:" + ars.getString(2));
-								resultH = 0;
-								resultA = 1;
+					while (hrs.next()) {
+						while (ars.next()) {
 
-							}
-							
 							double pointsbH = hrs.getInt(4);
 							double pointsbA = ars.getInt(4);
-							int important = 50;
-							
+
 							double expectedrH = 1 / (Math.pow(10, -(pointsbH - pointsbA) / 600) + 1);
 							double expectedrA = 1 / (Math.pow(10, -(pointsbA - pointsbH) / 600) + 1);
 
 							pointsbH = (int) Math.round(pointsbH + (important * (resultH - expectedrH)));
 							pointsbA = (int) Math.round(pointsbA + (important * (resultA - expectedrA)));
 
-							System.out.println("New points for " +hrs.getString(2)+ ": " + pointsbH);
-							System.out.println("New points for " +ars.getString(2)+ ": " + pointsbA);
+							System.out.println("New points for " + hrs.getString(2) + ": " + pointsbH);
+							System.out.println("New points for " + ars.getString(2) + ": " + pointsbA);
 
+							// up.setDouble(1, pointsbH); //home update
+							// up.setString(2, hrs.getString(2));
+							// up.executeUpdate();
 
+							// up.setDouble(1, pointsbA); //away update
+							// up.setString(2, hrs.getString(2));
+							// up.executeUpdate();
 
-						//	up.setInt(1, rs2.getInt(4)+hp);
-						//	up.setString(2, rs2.getString(2));
-						//	up.executeUpdate();
 						}
-						}
-					}				
+					}
+				}
 				conn.close();
 			} catch (Exception e) {
 				System.out.print("Do not connect to DB - Error:" + e);
 			}
 		}
-
-	}
-
-	public static void setNewFIFAPoints() {
-		// need the correct points here and it's ready
-		int pointsbH = 0;
-		int pointsbA = 0;
-		int important = 0;
-		int resultH = 0;
-		int resultA = 0;
-		
-		double expectedrH = 1 / (Math.pow(10, -(pointsbH - pointsbA) / 600) + 1);
-		double expectedrA = 1 / (Math.pow(10, -(pointsbA - pointsbH) / 600) + 1);
-
-		pointsbH = (int) Math.round(pointsbH + (important * (resultH - expectedrH)));
-		pointsbA = (int) Math.round(pointsbA + (important * (resultA - expectedrA)));
-
-		System.out.println("Home team new points: " + pointsbH);
-		System.out.println("Away team new points: " + pointsbA);
 	}
 }
--- World Cup/src/Simulate.java
+++ World Cup/src/Simulate.java
@@ -38,12 +38,15 @@ public class Simulate {
 
 					if (h > a) {
 						System.out.println("The winner is " + rs.getString(2));
+						FIFAPoints.setPoints(id, 50, 1,0);
 
 					} else if (h == a) {
 						System.out.println("Draw");
+						FIFAPoints.setPoints(id, 50, 0.5,0.5);
 
 					} else {
 						System.out.println("The winner is " + rs.getString(3));
+						FIFAPoints.setPoints(id, 50, 0,1);
 
 					}
 				}
@@ -57,29 +60,29 @@ public class Simulate {
 
 	public static void Roundof16() {
 		for (int id = 149; id <= 156; id++) {
-			KnockOutScore(id);
+			KnockOutScore(id,50);
 		}
 	}
 
 	public static void QuarterFinals() {
 		for (int id = 157; id <= 160; id++) {
-			KnockOutScore(id);
+			KnockOutScore(id,60);
 		}
 	}
 
 	public static void SemiFinals() {
 		for (int id = 161; id <= 162; id++) {
-			KnockOutScore(id);
+			KnockOutScore(id,60);
 		}
 	}
 
 	public static void Final() {
 		for (int id = 163; id <= 164; id++) {
-			KnockOutScore(id);
+			KnockOutScore(id,60);
 		}
 	}
 
-	public static void KnockOutScore(int id) {
+	public static void KnockOutScore(int id, int important) {
 		Random n = new Random();
 
 		String q = "SELECT * FROM schedule WHERE Match_ID= " + id;
@@ -104,25 +107,27 @@ public class Simulate {
 				System.out.println(rs.getString(2) + " vs " + rs.getString(3));
 
 				if (h > a) { // winner h in 90'
-					System.out
-							.println("The winner is " + rs.getString(2) + " in 90' and the score is: " + h + " - " + a);
+					System.out.println("The winner is " + rs.getString(2) + " in 90' and the score is: " + h + " - " + a);
 
 					up.setInt(1, h);
 					up.setInt(2, a);
 					up.setInt(3, id);
 					up.executeUpdate();
+					FIFAPoints.setPoints(id, important, 1,0);
+
 				} else if (h == a) { // draw in 90'
 					int hh = n.nextInt(4);
 					int aa = n.nextInt(4);
 					if (hh > aa) { // winner h in 120'
 						System.out.println("The score in 90' was: " + h + " - " + a);
-						System.out.println("The winner is " + rs.getString(2) + " in 120' and the score is: " + (hh + h)
-								+ " - " + (aa + a));
+						System.out.println("The winner is " + rs.getString(2) + " in 120' and the score is: " + (hh + h) + " - " + (aa + a));
 
 						up.setInt(1, hh + h);
 						up.setInt(2, aa + a);
 						up.setInt(3, id);
 						up.executeUpdate();
+						FIFAPoints.setPoints(id, important, 1,0);
+
 					} else if (hh == aa) { // draw in 120'
 						do {
 							ph = n.nextInt(6);
@@ -131,24 +136,26 @@ public class Simulate {
 
 						if (ph > pa) {
 							System.out.println("The score in 90' was: " + h + " - " + a);
-							System.out.println(
-									"The winner is " + rs.getString(2) + " in Penalty shoot-out and the score is: "
+							System.out.println("The winner is " + rs.getString(2) + " in Penalty shoot-out and the score is: "
 											+ (hh + h) + "(" + ph + ") - " + (aa + a) + "(" + pa + ")");
 
 							up.setInt(1, ((hh + h) + 1));
 							up.setInt(2, aa + a);
 							up.setInt(3, id);
 							up.executeUpdate();
+							FIFAPoints.setPoints(id, important, 0.75,0.5);
+
 						} else {
 							System.out.println("The score in 90' was: " + h + " - " + a);
-							System.out.println(
-									"The winner is " + rs.getString(3) + " in Penalty shoot-out and the score is: "
+							System.out.println("The winner is " + rs.getString(3) + " in Penalty shoot-out and the score is: "
 											+ (hh + h) + "(" + ph + ") - " + (aa + a) + "(" + pa + ")");
 
 							up.setInt(1, hh + h);
 							up.setInt(2, ((aa + a) + 1));
 							up.setInt(3, id);
 							up.executeUpdate();
+							FIFAPoints.setPoints(id, important, 0.5,0.75);
+
 						}
 					} else {// winner a in 120'
 						System.out.println("The score in 90' was: " + h + " - " + a);
@@ -159,14 +166,17 @@ public class Simulate {
 						up.setInt(2, aa + a);
 						up.setInt(3, id);
 						up.executeUpdate();
+						FIFAPoints.setPoints(id, important, 0,1);
+
 					}
 				} else { // winner a in 90'
-					System.out
-							.println("The winner is " + rs.getString(3) + " in 90' and the score is: " + h + " - " + a);
+					System.out.println("The winner is " + rs.getString(3) + " in 90' and the score is: " + h + " - " + a);
 					up.setInt(1, h);
 					up.setInt(2, a);
 					up.setInt(3, id);
 					up.executeUpdate();
+					FIFAPoints.setPoints(id, important, 0,1);
+
 				}
 			}
 			conn.close();
