aa1441fde4566cbdc221324f5a0f270809673b42
GeorgiouIlias <66827818+georgiouilias@users.noreply.github.com>
2020-09-18 15:36:52 +0300

    insert if for double name

--- World Cup/src/Insert.java
+++ World Cup/src/Insert.java
@@ -6,7 +6,7 @@ import java.sql.Savepoint;
 
 public class Insert {
 	public static void InsertTeams() {
-		int teams = 32;
+		int teams = 2;
 		for (int i = 0; i < teams; i++) {
 			int j = i + 1;
 			int c = 0;
@@ -15,6 +15,7 @@ public class Insert {
 			String cname = UserInput.getString();
 
 			String query0 = "INSERT INTO worldcupfinal (ID, Team, Confederation) " + "VALUES (?, ?, ?)";
+			String query1 = "SELECT Team, Confederation FROM worldcupfinal WHERE Team = '" + cname + "'";
 			String query2 = "SELECT Name, Confederation FROM fifaranking WHERE Name = '" + cname + "'";
 
 			try {
@@ -25,21 +26,44 @@ public class Insert {
 				PreparedStatement pst0 = conn.prepareStatement(query0);
 				// pst0.executeQuery();
 
+				PreparedStatement pst1 = conn.prepareStatement(query1);
+				ResultSet rs1 = pst1.executeQuery();
+
 				PreparedStatement pst2 = conn.prepareStatement(query2);
 				ResultSet rs2 = pst2.executeQuery();
 
 				Savepoint svp1 = conn.setSavepoint();
 
 				while (rs2.next()) {
-					if (rs2.getString(1).equals(cname)) {
+					if (rs2.getString(1).equals(cname)) {// checks if the team is in the fifa ranking
+						System.out.println("Check after if and after first sql check!\n");
+						if (rs1.next() == false) {
+							pst0.setInt(1, j);
+							pst0.setString(2, cname);
+							pst0.setString(3, rs2.getString(2));
+							pst0.execute();
 
-						pst0.setInt(1, j);
-						pst0.setString(2, cname);
-						pst0.setString(3, rs2.getString(2));
-						pst0.execute();
+							System.out.println("Team added! (" + j + "/" + teams + ")\n");
 
-						System.out.println("Team added! (" + j + "/" + teams + ")\n");
+						} else {
+							System.out.println("Check else!\n");
+							System.out.println("Check before if and after second sql check!\n");
+							if (rs1.getString(1).equals(cname)) {
+								System.out.println("This team has been added already!\n");
+								conn.rollback(svp1);
+								conn.commit();
+							} else {
+								System.out.println("ekso apo tin eisodo\n");
+								pst0.setInt(1, j);
+								pst0.setString(2, cname);
+								pst0.setString(3, rs2.getString(2));
+								pst0.execute();
+
+								System.out.println("Team added! (" + j + "/" + teams + ")\n");
+							}
+						}
 					}
+
 					c++;
 				}
 				conn.setAutoCommit(false);
